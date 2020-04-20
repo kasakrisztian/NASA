23dfc60237f8eaf53feda6562525257307759fc1
kasakrisztian <kasakrisztian20@gmail.com>
2020-04-20 10:29:47 +0200

    commit

--- src/__tests__/Kérés.test.ts
+++ src/__tests__/Kérés.test.ts
@@ -2,6 +2,7 @@ import Kérés from "../Kérés";
 
 describe("Kérés osztály unit tesztek: ", () => {
     const instance: Kérés = new Kérés("nccsmm.gsfc.nasa.gov*20/Jul/1995:00:55:34*GET /shuttle/technology/images/et_1.jpg*200 144114");
+    const instance2: Kérés = new Kérés("pm151.smartlink.net*20/Jul/1995:00:13:01*GET /shuttle/missions/sts-70/images/DSC-95EC-0001.jpg*304 0");
 
     it("Kérés osztálypéldány ellenőrzése", async () => {
         expect(instance).toBeInstanceOf(Kérés);
@@ -12,7 +13,7 @@ describe("Kérés osztály unit tesztek: ", () => {
     });
 
     it("Állapotkód", async () => {
-        expect(instance.állapotkód).toBe("304");
+        expect(instance2.állapotkód).toBe("304");
     });
 
     it("Byte mérete", async () => {
@@ -20,18 +21,10 @@ describe("Kérés osztály unit tesztek: ", () => {
     });
 
     it("Byte mérete", async () => {
-        expect(instance.byteMéret).toBe(488494);
-    });
-
-    it("Byte mérete", async () => {
-        expect(instance.byteMéret).toBe(0);
+        expect(instance2.byteMéret).toBe(0);
     });
 
     it("Domain cím-e", async () => {
         expect(instance.domain).toBe(true);
-    });
-
-    it("Domain cím-e", async () => {
-        expect(instance.domain).toBe(false);
     });
 });
